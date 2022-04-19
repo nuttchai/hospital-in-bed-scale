0b7d3cc00bf5fcb2e117d872be1e045ca419ce7c
Nutt Chairatana <nuttchai@outlook.com>
2022-04-19 07:38:06 +0700

    Add dropdown default text

--- src/constants/DescriptionText.js
+++ src/constants/DescriptionText.js
@@ -3,6 +3,7 @@ const DescriptionText = {
   last24Hrs: "Last 24 Hours",
   dateTime: "Date & Time Interval",
   timeOnly: "Time Interval",
+  defaultDropdownSelection: "Default",
 };
 
 export default DescriptionText;
