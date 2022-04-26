41fcf5e8b185d0e441dc69e52c515c68204526df
nuttchai <nuttchai@outlook.com>
2022-04-27 06:56:30 +0700

    Add React Tab Name

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 import Dashboard from "./pages/Dashboard/Dashboard";
 
 const App = () => {
+  useEffect(() => {
+    document.title = "Patient Weight Tracker";
+  }, []);
+
   return (
     <div className="app-container">
       <Dashboard />
